479c5edb1919f0dc9a015ab172f33e88ebec82f5
Auxile_Isaac <auxile.isaac@gmail.com>
2022-04-09 00:44:27 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main() 
 {
-	   // printf() displays the string inside quotation
+	   /*
+	    *  printf() displays the string inside quotation
+	    */
+
 	   printf("with proper grammar, but the outcome is a piece of art,");
 	   return 0
 }
