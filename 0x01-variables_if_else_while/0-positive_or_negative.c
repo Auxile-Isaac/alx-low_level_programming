68aa6cda7af44e1847b71a758faf7007f9fb8c20
Auxile_Isaac <auxile.isaac@gmail.com>
2022-04-10 05:54:19 -0700

    correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,9 @@ int main(void)
 	{
 		printf("is positive");
 	}else if(n < 0){
-		printf("is negative")
+		printf("is negative");
 	}else{
-		printf("is zero")
+		printf("is zero");
 	}
 	return (0);
 }
