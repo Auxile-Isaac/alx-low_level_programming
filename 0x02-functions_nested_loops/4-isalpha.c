5a58cdbeddf31c299aa8f4a4098c2b8b2a827109
Auxile_Isaac <auxile.isaac@gmail.com>
2022-04-12 08:02:42 -0700

    correction

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,9 +9,12 @@
 
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z') or (c >= 'A' && c <= 'B')
+	if (c >= 'a' && c <= 'z')
 	{
-		return (1);
+		if (c >= 'A' && c <= 'Z')
+		{
+			return (1);
+		}
 	}
 	else
 	{
