bbfc52b0c6dda8ff97fa05f6cba49c4a41949ca6
Auxile_Isaac <auxile.isaac@gmail.com>
2022-04-12 08:11:54 -0700

    correction

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * isalpha - checks for alphabets
+ * isalpha - checks for alphabetic character
  * @c: c is an ascii character
  *
- * Return: 1 if lowercase or upcase
+ * Return: 1 (if letter)
  */
 
 int _isalpha(int c)
