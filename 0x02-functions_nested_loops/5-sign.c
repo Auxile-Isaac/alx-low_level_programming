711727f2fc005a309ef4cad458157c156b6a437a
Auxile_Isaac <auxile.isaac@gmail.com>
2022-04-12 08:41:45 -0700

    5-sign.c added

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * int print_sign(int n) - checks if number > 0
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		printf("+");
+	}
+	else if (n = 0)
+	{
+		return (0);
+		printf("0");
+	}
+	else
+	{
+		return (-);
+		printf(-1);
+	}
+}
